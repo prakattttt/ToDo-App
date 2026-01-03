a82e3f9a77e211a9e8d6ab6ae0aedb353c975793
prakat <prakat.dhakall64@gmail.com>
2026-01-03 14:44:52 +0545

    added styling to the header

--- client/src/components/Head.jsx
+++ client/src/components/Head.jsx
@@ -1,11 +1,13 @@
+import logo from "../assets/checklist.png"
+
 const Head = () => {
   return (
-    <header>
-      <div className="logo">
-        <img src="" alt="logo" />
-        <span>To Do App</span>
+    <header className="center flex-col m-5">
+      <div className="center mb-1">
+        <img src={logo} className="w-12" alt="logo" />
+        <span className="text-2xl font-bold ml-1">To Do App</span>
       </div>
-      <p className="light-text">
+      <p className="text-[0.75rem] text-gray-700">
         Stay Organized and manage your tasks effeciently
       </p>
     </header>
