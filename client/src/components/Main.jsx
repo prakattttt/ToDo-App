1e749247d824681b630a0edc3574bff44257c661
prakat <prakat.dhakall64@gmail.com>
2026-01-03 16:08:13 +0545

    styled the main component

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,29 +1,64 @@
 const Main = () => {
   return (
-    <main>
-        <div className="searchbar">
-            <input type="search" name="task" id="task" />
-            <button>+</button>
-        </div>
-        <div className="categories">
-            <div className="box">All (3)</div>
-            <div className="box">Active (1)</div>
-            <div className="box">Completed (2)</div>
-        </div>
-        <div className="tasks">
-            <ul>
-                <li><input type="radio" name="task" id="task1" />Create a responsive design</li>
-                <li><input type="radio" name="task" id="task2" />Add task filtering</li>
-                <li><input type="radio" name="task" id="task3" />Implement Database</li>
-            </ul>
-        </div>
-        <hr />
-        <div className="bottom-main">
-            <p>2 tasks remaining</p>
-            <p>1 completed</p>
-        </div>
+    <main className="bg-white max-w-4xl m-auto p-5 rounded-lg mt-8">
+      <div className="center">
+        <input
+          className="bg-gray-100 w-full h-8 p-1 text-[1rem] rounded-md pl-2"
+          type="search"
+          name="task"
+          id="task"
+          placeholder="Add a new task..."
+        />
+        <button
+          className="ml-2 h-8 w-9 bg-black text-white rounded-md
+               flex items-center justify-center text-xl leading-none"
+        >
+          +
+        </button>
+      </div>
+      <div className="my-3 flex gap-3">
+        <div className="box selected">All (3)</div>
+        <div className="box">Active (1)</div>
+        <div className="box">Completed (2)</div>
+      </div>
+      <div className="tasks">
+        <ul className="mt-4">
+          <li className="list flex items-center">
+            <input
+              className="checkbox center"
+              type="checkbox"
+              name="task"
+              id="task1"
+            />
+            Create a responsive design
+          </li>
+          <li className="list flex items-center">
+            <input
+              className="checkbox"
+              type="checkbox"
+              name="task"
+              id="task2"
+            />
+            Add task filtering
+          </li>
+          <li className="list flex items-center">
+            <input
+              className="checkbox"
+              type="checkbox"
+              name="task"
+              id="task3"
+            />
+            Implement Database
+          </li>
+        </ul>
+      </div>
+      <hr className="border-t-2 border-gray-100 mt-4"/>
+      <div className="bottom-main flex items-center justify-between mt-3">
+        <p>2 tasks remaining</p>
+        <p>1 completed</p>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
