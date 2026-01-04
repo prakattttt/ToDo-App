9623b15ff97b3d0080a1065f934fd2e176a89486
prakat <prakat.dhakall64@gmail.com>
2026-01-04 20:48:38 +0545

    changed fonts

--- client/src/components/Head.jsx
+++ client/src/components/Head.jsx
@@ -5,7 +5,7 @@ const Head = () => {
     <header className="center flex-col m-8">
       <div className="center mb-2">
         <img src={logo} className="w-12" alt="logo" />
-        <span className="text-2xl font-bold ml-1">Todo App</span>
+        <span className="text-3xl font-bold ml-1 font-ubuntu">Todo App</span>
       </div>
       <p className="text-[1rem] text-gray-500 text-center max-sm:text-[0.85rem]">
         Stay Organized and manage your tasks effeciently
