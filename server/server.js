46f1a100c90d24c7ce80e44ab58fbe284947f018
prakat <prakat.dhakall64@gmail.com>
2026-01-20 14:28:42 +0545

    allow cookies from backend to frontened

--- server/server.js
+++ server/server.js
@@ -21,7 +21,8 @@ app.use(
   cors({
     origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
   })
 );
 
